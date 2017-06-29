eec932cf0b28757aa164c7210277c4c1b7e21d41
elShunior <zahoz.moopa@gmail.com>
2017-06-29 11:21:56 -0500

    icon head

--- index.php
+++ index.php
@@ -73,6 +73,13 @@ if (isset($_POST['login'])) {
   <!-- iCheck -->
   <link rel="stylesheet" href="admin/plugins/iCheck/square/blue.css">
 
+
+  <!--
+      logo icon
+  -->
+  <link rel="shortcut icon" type="image/png" href="admin/dist/img/icon.png"/>
+
+
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
