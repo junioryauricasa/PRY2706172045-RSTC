b71950690314f1e831469550441665b507e90cf8
hvivanconunez <hvivanconunez@gmail.com>
2017-10-29 21:27:32 -0500

    Desarrollo de los Submódulos de Monedas Parte 1.1
    Se desarrollo con las clases de éstos submódulos.

---
+++ datos/administrativo/clases_moneda_comercial/class_moneda_comercial.php
@@ -0,0 +1,251 @@
+<?php
+require_once '../conexion/bd_conexion.php';
+require_once 'class_formulario_moneda_comercial.php';
+class MonedaComercial
+{
+  /* INICIO - Atributos de MonedaComercial*/
+  private $intIdMonedaComercial;
+  private $intIdTipoCambio;
+  private $dcmCambio1;
+  private $dcmCambio2;
+  private $dtmFechaCambio;
+  
+  public function IdMonedaComercial($intIdMonedaComercial){ $this->intIdMonedaComercial = $intIdMonedaComercial; }
+  public function IdTipoCambio($intIdTipoCambio){ $this->intIdTipoCambio = $intIdTipoCambio; }
+  public function Cambio1($dcmCambio1){ $this->dcmCambio1 = $dcmCambio1; }
+  public function Cambio2($dcmCambio2){ $this->dcmCambio2 = $dcmCambio2; }
+  public function FechaCambio($dtmFechaCambio){ $this->dtmFechaCambio = $dtmFechaCambio; }
+  /* FIN - Atributos de MonedaComercial */
+
+  /* INICIO - Métodos de MonedaComercial */
+  public function InsertarMonedaComercial()
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL insertarMonedaComercial(@intIdMonedaComercial,:intIdTipoCambio,:dcmCambio1,:dcmCambio2,
+        :dtmFechaCambio)');
+      $sql_comando->execute(array(
+        ':intIdTipoCambio' => $this->intIdTipoCambio,
+        ':dcmCambio1' => $this->dcmCambio1,
+        ':dcmCambio2' => $this->dcmCambio2,
+        ':dtmFechaCambio' => $this->dtmFechaCambio));
+      $sql_comando->closeCursor();
+      $salidas = $sql_conectar->query("select @intIdMonedaComercial as intIdMonedaComercial");
+      $salida = $salidas->fetchObject();
+      $_SESSION['intIdMonedaComercial'] = $salida->intIdMonedaComercial;
+      echo "ok";
+    }
+    catch(PDPExceptions $e){
+      echo $e->getMessage();
+    }
+  }
+
+  public function MostrarMonedaComercial($funcion)
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL mostrarMonedaComercial(:intIdMonedaComercial)');
+      $sql_comando -> execute(array(':intIdMonedaComercial' => $this->intIdMonedaComercial));
+      $fila = $sql_comando -> fetch(PDO::FETCH_ASSOC);
+
+      $FormularioMonedaComercial = new FormularioMonedaComercial();
+      $FormularioMonedaComercial->IdTipoCambio($fila['intIdTipoCambio']);
+      $FormularioMonedaComercial->Cambio1($fila['dcmCambio1']);
+      $FormularioMonedaComercial->Cambio2($fila['dcmCambio2']);
+      $FormularioMonedaComercial->FechaCambio($fila['dtmFechaCambio']);
+      $FormularioMonedaComercial->ConsultarFormulario($funcion);
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }    
+  }
+
+  public function ActualizarMonedaComercial()
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL actualizarMonedaComercial(:intIdMonedaComercial,
+        :intIdTipoCambio,:dcmCambio1,:dcmCambio2,:dtmFechaCambio)');
+      $sql_comando->execute(array(
+        ':intIdMonedaComercial' => $this->intIdMonedaComercial,
+        ':intIdTipoCambio' => $this->intIdTipoCambio,
+        ':dcmCambio1' => $this->dcmCambio1,
+        ':dcmCambio2' => $this->dcmCambio2,
+        ':dtmFechaCambio' => $this->dtmFechaCambio));
+      $_SESSION['intIdMonedaComercial'] = $this->intIdMonedaComercial;
+      $_SESSION['RutaDefaultImg'] = "";
+      echo "ok";
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }
+  }
+
+  public function EliminarMonedaComercial()
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL eliminarMonedaComercial(:intIdMonedaComercial)');
+      $sql_comando -> execute(array(':intIdMonedaComercial' => $this->intIdMonedaComercial));
+      $_SESSION['intIdMonedaComercial'] = $this->intIdMonedaComercial;
+      echo 'ok';
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }
+  }
+
+  public function ListarMonedaComercial(,$x,$y,$tipolistado,$TipoCambio)
+  {
+    try{
+      $residuo = 0;
+      $cantidad = 0;
+      $numpaginas = 0;
+      $i = 0;
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      //Busqueda de MonedaComercial por el comando LIMIT
+      if($tipolistado == "N"){
+        $sql_comando = $sql_conectar->prepare('CALL buscarMonedaComercial_ii(:TipoCambio)');
+        $sql_comando -> execute(array(':TipoCambio' => $TipoCambio));
+        $cantidad = $sql_comando -> rowCount();
+        $numpaginas = ceil($cantidad / $y);
+        $x = ($numpaginas - 1) * $y;
+        $i = 1;
+      } else if ($tipolistado == "D"){
+        $sql_comando = $sql_conectar->prepare('CALL buscarMonedaComercial_ii(:TipoCambio)');
+        $sql_comando -> execute(array(':TipoCambio' => $TipoCambio));
+        $cantidad = $sql_comando -> rowCount();
+        $residuo = $cantidad % $y;
+        if($residuo == 0)
+        {$x = $x - $y;}
+      }
+      //Busqueda de MonedaComercial por el comando LIMIT
+      $sql_comando = $sql_conectar->prepare('CALL buscarMonedaComercial(:TipoCambio)');
+      $sql_comando -> execute(array(':x' => $x,':y' => $y, ':TipoCambio' => $TipoCambio));
+      $numpaginas = ceil($cantidad / $y);
+      while($fila = $sql_comando -> fetch(PDO::FETCH_ASSOC))
+      {
+        if($fila["nvchCodigo"]!=""){
+          if($i == ($cantidad - $x) && $tipolistado == "N"){
+            echo '<tr bgcolor="#BEE1EB">';
+          } else if($fila["intIdMonedaComercial"] == $_SESSION['intIdMonedaComercial'] && $tipolistado == "E"){
+            echo '<tr bgcolor="#B3E4C0">';
+          }else {
+            echo '<tr>';
+          }
+          echo 
+          '<td>'.$fila["dtmFechaCambio"].'</td>
+          <td>'.$fila["dcmCambio1"].'</td>
+          <td>'.$fila["dcmCambio2"].'</td>
+          <td> 
+            <button type="button" id="'.$fila["intIdMonedaComercial"].'" class="btn btn-xs btn-warning btn-mostrar-moneda-comercial">
+              <i class="fa fa-edit"></i> Editar
+            </button>
+            <button type="button" id="'.$fila["intIdMonedaComercial"].'" class="btn btn-xs btn-danger btn-eliminar-moneda-comercial">
+              <i class="fa fa-edit"></i> Eliminar
+            </button>
+          </td>  
+          </tr>';
+          $i++;
+        }
+      }
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }  
+  }
+
+  public function PaginarMonedaComercial($x,$y,$tipolistado,$TipoCambio)
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL buscarMonedaComercial_ii(:TipoCambio)');
+      $sql_comando -> execute(array(':TipoCambio' => $TipoCambio));
+      $cantidad = $sql_comando -> rowCount();
+      $numpaginas = ceil($cantidad / $y);
+      if($tipolistado == "N" || $tipolistado == "D")
+      { $x = $numpaginas - 1; }
+      else if($tipolistado == "E")
+      { $x = $x / $y; }
+      $output = "";
+      for($i = 0; $i < $numpaginas; $i++){
+        if($i==0)
+        {
+          if($x==0)
+          {
+            $output .= 
+            '<li class="page-item disabled">
+                <a class="page-link" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Anterior</span>
+                </a>
+            </li>';
+          } else {
+            $output .= 
+            '<li class="page-item">
+                <a idp="'.($x-1).'" class="page-link btn-pagina" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Anterior</span>
+                </a>
+            </li>';
+          }
+        }
+
+          if($x==$i){
+            $output.=  '<li class="page-item active"><a idp="'.$i.'" class="page-link btn-pagina marca">'.($i+1).'</a></li>';
+          }
+          else
+          {
+            $output.=  '<li class="page-item"><a idp="'.$i.'" class="page-link btn-pagina">'.($i+1).'</a></li>';
+          }
+
+        if($i==($numpaginas-1))
+        {
+          if($x==($numpaginas-1))
+          {
+            $output .= 
+            '<li class="page-item disabled">
+                <a class="page-link" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Siguiente</span>
+                </a>
+            </li>';
+          } else {
+            $output .= 
+            '<li class="page-item">
+                <a idp="'.($x+1).'" class="page-link btn-pagina" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Siguiente</span>
+                </a>
+            </li>';
+          }
+        }
+      }
+      if($output == ""){
+        $output .= 
+            '<li class="page-item">
+                <a class="page-link btn-pagina" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Anterior</span>
+                </a>
+            </li>';
+        $output .= 
+            '<li class="page-item">
+                <a class="page-link btn-pagina" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Siguiente</span>
+                </a>
+            </li>';
+      }
+      echo $output;
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }  
+  }
---
+++ datos/administrativo/clases_moneda_tributaria/class_moneda_tributaria.php
@@ -0,0 +1,251 @@
+<?php
+require_once '../conexion/bd_conexion.php';
+require_once 'class_formulario_moneda_tributaria.php';
+class MonedaTributaria
+{
+  /* INICIO - Atributos de MonedaTributaria*/
+  private $intIdMonedaTributaria;
+  private $intIdTipoCambio;
+  private $dcmCambio1;
+  private $dcmCambio2;
+  private $dtmFechaCambio;
+  
+  public function IdMonedaTributaria($intIdMonedaTributaria){ $this->intIdMonedaTributaria = $intIdMonedaTributaria; }
+  public function IdTipoCambio($intIdTipoCambio){ $this->intIdTipoCambio = $intIdTipoCambio; }
+  public function Cambio1($dcmCambio1){ $this->dcmCambio1 = $dcmCambio1; }
+  public function Cambio2($dcmCambio2){ $this->dcmCambio2 = $dcmCambio2; }
+  public function FechaCambio($dtmFechaCambio){ $this->dtmFechaCambio = $dtmFechaCambio; }
+  /* FIN - Atributos de MonedaTributaria */
+
+  /* INICIO - Métodos de MonedaTributaria */
+  public function InsertarMonedaTributaria()
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL insertarMonedaTributaria(@intIdMonedaTributaria,:intIdTipoCambio,:dcmCambio1,:dcmCambio2,
+        :dtmFechaCambio)');
+      $sql_comando->execute(array(
+        ':intIdTipoCambio' => $this->intIdTipoCambio,
+        ':dcmCambio1' => $this->dcmCambio1,
+        ':dcmCambio2' => $this->dcmCambio2,
+        ':dtmFechaCambio' => $this->dtmFechaCambio));
+      $sql_comando->closeCursor();
+      $salidas = $sql_conectar->query("select @intIdMonedaTributaria as intIdMonedaTributaria");
+      $salida = $salidas->fetchObject();
+      $_SESSION['intIdMonedaTributaria'] = $salida->intIdMonedaTributaria;
+      echo "ok";
+    }
+    catch(PDPExceptions $e){
+      echo $e->getMessage();
+    }
+  }
+
+  public function MostrarMonedaTributaria($funcion)
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL mostrarMonedaTributaria(:intIdMonedaTributaria)');
+      $sql_comando -> execute(array(':intIdMonedaTributaria' => $this->intIdMonedaTributaria));
+      $fila = $sql_comando -> fetch(PDO::FETCH_ASSOC);
+
+      $FormularioMonedaTributaria = new FormularioMonedaTributaria();
+      $FormularioMonedaTributaria->IdTipoCambio($fila['intIdTipoCambio']);
+      $FormularioMonedaTributaria->Cambio1($fila['dcmCambio1']);
+      $FormularioMonedaTributaria->Cambio2($fila['dcmCambio2']);
+      $FormularioMonedaTributaria->FechaCambio($fila['dtmFechaCambio']);
+      $FormularioMonedaTributaria->ConsultarFormulario($funcion);
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }    
+  }
+
+  public function ActualizarMonedaTributaria()
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL actualizarMonedaTributaria(:intIdMonedaTributaria,
+        :intIdTipoCambio,:dcmCambio1,:dcmCambio2,:dtmFechaCambio)');
+      $sql_comando->execute(array(
+        ':intIdMonedaTributaria' => $this->intIdMonedaTributaria,
+        ':intIdTipoCambio' => $this->intIdTipoCambio,
+        ':dcmCambio1' => $this->dcmCambio1,
+        ':dcmCambio2' => $this->dcmCambio2,
+        ':dtmFechaCambio' => $this->dtmFechaCambio));
+      $_SESSION['intIdMonedaTributaria'] = $this->intIdMonedaTributaria;
+      $_SESSION['RutaDefaultImg'] = "";
+      echo "ok";
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }
+  }
+
+  public function EliminarMonedaTributaria()
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL eliminarMonedaTributaria(:intIdMonedaTributaria)');
+      $sql_comando -> execute(array(':intIdMonedaTributaria' => $this->intIdMonedaTributaria));
+      $_SESSION['intIdMonedaTributaria'] = $this->intIdMonedaTributaria;
+      echo 'ok';
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }
+  }
+
+  public function ListarMonedaTributaria(,$x,$y,$tipolistado,$TipoCambio)
+  {
+    try{
+      $residuo = 0;
+      $cantidad = 0;
+      $numpaginas = 0;
+      $i = 0;
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      //Busqueda de MonedaTributaria por el comando LIMIT
+      if($tipolistado == "N"){
+        $sql_comando = $sql_conectar->prepare('CALL buscarMonedaTributaria_ii(:TipoCambio)');
+        $sql_comando -> execute(array(':TipoCambio' => $TipoCambio));
+        $cantidad = $sql_comando -> rowCount();
+        $numpaginas = ceil($cantidad / $y);
+        $x = ($numpaginas - 1) * $y;
+        $i = 1;
+      } else if ($tipolistado == "D"){
+        $sql_comando = $sql_conectar->prepare('CALL buscarMonedaTributaria_ii(:TipoCambio)');
+        $sql_comando -> execute(array(':TipoCambio' => $TipoCambio));
+        $cantidad = $sql_comando -> rowCount();
+        $residuo = $cantidad % $y;
+        if($residuo == 0)
+        {$x = $x - $y;}
+      }
+      //Busqueda de MonedaTributaria por el comando LIMIT
+      $sql_comando = $sql_conectar->prepare('CALL buscarMonedaTributaria(:TipoCambio)');
+      $sql_comando -> execute(array(':x' => $x,':y' => $y, ':TipoCambio' => $TipoCambio));
+      $numpaginas = ceil($cantidad / $y);
+      while($fila = $sql_comando -> fetch(PDO::FETCH_ASSOC))
+      {
+        if($fila["nvchCodigo"]!=""){
+          if($i == ($cantidad - $x) && $tipolistado == "N"){
+            echo '<tr bgcolor="#BEE1EB">';
+          } else if($fila["intIdMonedaTributaria"] == $_SESSION['intIdMonedaTributaria'] && $tipolistado == "E"){
+            echo '<tr bgcolor="#B3E4C0">';
+          }else {
+            echo '<tr>';
+          }
+          echo 
+          '<td>'.$fila["dtmFechaCambio"].'</td>
+          <td>'.$fila["dcmCambio1"].'</td>
+          <td>'.$fila["dcmCambio2"].'</td>
+          <td> 
+            <button type="button" id="'.$fila["intIdMonedaTributaria"].'" class="btn btn-xs btn-warning btn-mostrar-moneda-tributaria">
+              <i class="fa fa-edit"></i> Editar
+            </button>
+            <button type="button" id="'.$fila["intIdMonedaTributaria"].'" class="btn btn-xs btn-danger btn-eliminar-moneda-tributaria">
+              <i class="fa fa-edit"></i> Eliminar
+            </button>
+          </td>  
+          </tr>';
+          $i++;
+        }
+      }
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }  
+  }
+
+  public function PaginarMonedaTributaria($x,$y,$tipolistado,$TipoCambio)
+  {
+    try{
+      $sql_conexion = new Conexion_BD();
+      $sql_conectar = $sql_conexion->Conectar();
+      $sql_comando = $sql_conectar->prepare('CALL buscarMonedaTributaria_ii(:TipoCambio)');
+      $sql_comando -> execute(array(':TipoCambio' => $TipoCambio));
+      $cantidad = $sql_comando -> rowCount();
+      $numpaginas = ceil($cantidad / $y);
+      if($tipolistado == "N" || $tipolistado == "D")
+      { $x = $numpaginas - 1; }
+      else if($tipolistado == "E")
+      { $x = $x / $y; }
+      $output = "";
+      for($i = 0; $i < $numpaginas; $i++){
+        if($i==0)
+        {
+          if($x==0)
+          {
+            $output .= 
+            '<li class="page-item disabled">
+                <a class="page-link" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Anterior</span>
+                </a>
+            </li>';
+          } else {
+            $output .= 
+            '<li class="page-item">
+                <a idp="'.($x-1).'" class="page-link btn-pagina" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Anterior</span>
+                </a>
+            </li>';
+          }
+        }
+
+          if($x==$i){
+            $output.=  '<li class="page-item active"><a idp="'.$i.'" class="page-link btn-pagina marca">'.($i+1).'</a></li>';
+          }
+          else
+          {
+            $output.=  '<li class="page-item"><a idp="'.$i.'" class="page-link btn-pagina">'.($i+1).'</a></li>';
+          }
+
+        if($i==($numpaginas-1))
+        {
+          if($x==($numpaginas-1))
+          {
+            $output .= 
+            '<li class="page-item disabled">
+                <a class="page-link" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Siguiente</span>
+                </a>
+            </li>';
+          } else {
+            $output .= 
+            '<li class="page-item">
+                <a idp="'.($x+1).'" class="page-link btn-pagina" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Siguiente</span>
+                </a>
+            </li>';
+          }
+        }
+      }
+      if($output == ""){
+        $output .= 
+            '<li class="page-item">
+                <a class="page-link btn-pagina" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Anterior</span>
+                </a>
+            </li>';
+        $output .= 
+            '<li class="page-item">
+                <a class="page-link btn-pagina" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Siguiente</span>
+                </a>
+            </li>';
+      }
+      echo $output;
+    }
+    catch(PDPExceptio $e){
+      echo $e->getMessage();
+    }  
+  }
