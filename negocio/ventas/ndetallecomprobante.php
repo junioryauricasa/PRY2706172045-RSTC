ac37a2c9beedc64c92a30c0df4fa599f4d2b857a
hvivanconunez <hvivanconunez@gmail.com>
2017-11-08 04:27:40 -0500

    Desarrollo del Nuevo Comprobante
    Desarrollo del Nuevo Comprobante

---
+++ negocio/ventas/ndetallecomprobante.php
@@ -0,0 +1,360 @@
+<script>
+//////////////////////////////////////////////////////////////
+/* INICIO - Funcion Ajax - Listar Producto */
+function PaginacionProductos(seleccion) {
+	var y = 5;
+	var x = $(seleccion).attr("idprt") * y;
+	ListarProductosSeleccion(x,y);
+}
+
+$(document).on('keyup', '#BusquedaProducto', function(){
+	var y = 5;
+	var x = 0;
+	ListarProductosSeleccion(x,y);
+});
+
+$(document).on('change', '#tipo-busqueda', function(){
+	var y = 5;
+	var x = 0;
+	ListarProductosSeleccion(x,y);
+});
+/* FIN - Funciones Ajax - Listar Producto */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Listar Productos para la Selección */
+function ListarProductosSeleccion(x,y) {
+	var busqueda = document.getElementById("BusquedaProducto").value;
+	var funcion = "MPT";
+	var TipoBusqueda = document.getElementById("tipo-busqueda").value;
+	var intIdSucursal = document.getElementById("lugar-venta").value;
+	var intIdTipoMoneda = document.getElementById("tipo-moneda").value;
+	  $.ajax({
+	   url:"../../datos/ventas/funcion_venta.php",
+	   method:"POST",
+	   data:{busqueda:busqueda,funcion:funcion,x:x,y:y,TipoBusqueda:TipoBusqueda,intIdSucursal:intIdSucursal,
+	   		intIdTipoMoneda:intIdTipoMoneda},
+	   success:function(datos)
+	   {
+	   	$("#ListaDeProductosSeleccion").html(datos);
+	   	PaginarProductosSeleccion((x/y),y);
+	   }
+	});
+}
+/* FIN - Listar Productos para la Selección */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Paginar Clientes para la Selección */
+function PaginarProductosSeleccion(x,y) {
+	var busqueda = document.getElementById("BusquedaProducto").value;
+	var funcion = "PPT";
+	var TipoBusqueda = document.getElementById("tipo-busqueda").value;
+	  $.ajax({
+	   url:"../../datos/ventas/funcion_venta.php",
+	   method:"POST",
+	   data:{busqueda:busqueda,funcion:funcion,x:x,y:y,TipoBusqueda:TipoBusqueda},
+	   success:function(datos)
+	   {
+	   	$("#PaginacionDeProductos").html(datos);
+	   }
+	  });
+}
+/* FIN - Paginar Clientes para la Selección */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Listar Domicilios según Ingresa */
+function SeleccionarProducto(seleccion) {
+	var intIdProducto = $(seleccion).attr("idsprt");
+	var nvchCodigo = $("input[type=hidden][name='SnvchCodigo["+intIdProducto+"]']").val();
+	var nvchSimbolo = $("input[type=hidden][name='SnvchSimbolo["+intIdProducto+"]']").val();
+	var nvchDescripcion = $("input[type=hidden][name='SnvchDescripcion["+intIdProducto+"]']").val();
+	var intCantidad = $("input[type=text][name='SintCantidad["+intIdProducto+"]']").val();
+	var CantidadTotal = $("input[type=hidden][name='SCantidadTotal["+intIdProducto+"]']").val();
+	var dcmPrecio = $("input[type=hidden][name='SdcmPrecioVenta1["+intIdProducto+"]']").val();
+	var Descuento = $("input[type=text][name='SdcmDescuento["+intIdProducto+"]']").val();
+	var PrecioUnitario = $("input[type=text][name='SdcmPrecioLista["+intIdProducto+"]']").val();
+	var Total = $("input[type=text][name='SdcmTotal["+intIdProducto+"]']").val();
+
+	if(Descuento == "" || Descuento == null) {
+		MensajeNormal("Ingresar el Descuento del Producto que estás eligiendo",2);
+		return false;
+	} else if(intCantidad == "" || intCantidad == null) {
+		MensajeNormal("Ingresar la Cantidad del Producto que estás eligiendo",2);
+		return false;
+	}
+
+	$('#ListaDeProductosComprar').append('<tr>'+
+		'<td>'+'<input type="hidden" name="intIdProducto[]" value="'+intIdProducto+'"/>'+nvchCodigo+'</td>'+
+		'<td>'+nvchDescripcion+'</td>'+
+		'<td>'+'<input type="hidden" name="intCantidad[]" value="'+intCantidad+'"/>'+intCantidad+'</td>'+
+		'<td>'+'<input type="hidden" name="dcmPrecioUnitario[]" value="'+PrecioUnitario+'"/>'+
+		'<input type="hidden" name="dcmPrecio[]" value="'+dcmPrecio+'"/>'+
+		'<input type="hidden" name="dcmDescuento[]" value="'+Descuento+'"/>'+nvchSimbolo+' '+PrecioUnitario+'</td>'+
+		'<td>'+'<input type="hidden" name="dcmTotal[]" value="'+Total+'"/>'+nvchSimbolo+' '+Total+'</td>'+
+		'<td><button type="button" onclick="EliminarFila(this)" class="btn btn-xs btn-danger"><i class="fa fa-edit"></i> Eliminar</button></td>'+
+		'</tr>');
+}
+/* FIN - Listar Domicilios según Ingresa */
+////////////////////////////////////////////////////////////// 
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Listar Domicilios según Ingresa */
+function AgregarServicio() {
+	if(EsVacio("nvchDescripcionServicio") == false){
+		return false;
+	} else if(EsNumeroEntero("intCantidadServicio") == false){
+		return false;
+	} else if(EsDecimal("dcmPrecioUnitarioServicio")== false){
+		return false;
+	}
+
+	var nvchDescripcion = $("#nvchDescripcionServicio").val();
+	var intCantidad = $("#intCantidadServicio").val();
+	var dcmPrecioUnitario = Number($("#dcmPrecioUnitarioServicio").val()).toFixed(2);
+	var Total = (dcmPrecioUnitario * intCantidad).toFixed(2); 
+
+	$('#ListaDeServiciosComprar').append('<tr>'+
+		'<td>'+'<input type="hidden" name="intCantidad[]" value="'+intCantidad+'"/>'+intCantidad+'</td>'+
+		'<td>'+'<input type="hidden" name="nvchDescripcionServicio[]" value="'+nvchDescripcion+'"/>'+nvchDescripcion+'</td>'+
+		'<td>'+'<input type="hidden" name="dcmPrecioUnitario[]" value="'+dcmPrecioUnitario+'"/>'+dcmPrecioUnitario+'</td>'+
+		'<td>'+'<input type="hidden" name="dcmTotal[]" value="'+Total+'"/>'+Total+'</td>'+
+		'<td><button type="button" onclick="EliminarFila(this)" class="btn btn-xs btn-danger"><i class="fa fa-edit"></i> Eliminar</button></td>'+
+		'</tr>');
+	RestablecerValidacion("nvchDescripcionServicio",1);
+	RestablecerValidacion("intCantidadServicio",1);
+	RestablecerValidacion("dcmPrecioUnitarioServicio",1);
+}
+/* FIN - Listar Domicilios según Ingresa */
+////////////////////////////////////////////////////////////// 
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Eliminar Fila Seleccionada */
+function EliminarFila(btn) {
+	var fila = btn.parentNode.parentNode;
+  	fila.parentNode.removeChild(fila);
+}
+/* FIN - Eliminar Fila Seleccionada */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Mostrar Detalle Orden Compra Seleccionado */
+function MostrarDetalleVenta(intIdVenta,tipolistado) {
+	var funcion = "MDV";
+	  $.ajax({
+	   url:"../../datos/ventas/funcion_venta.php",
+	   method:"POST",
+	   data:{intIdVenta:intIdVenta,funcion:funcion,tipolistado:tipolistado},
+	   success:function(datos)
+	   {
+	   	if($("#intIdTipoVenta").val() == 1){
+	   		$("#ListaDeProductosComprar").html(datos);
+	   	} else if($("#intIdTipoVenta").val() == 2){
+	   		$("#ListaDeServiciosComprar").html(datos);
+	   	}
+	   }
+	  });
+}
+/* FIN - Mostrar Detalle Orden Compra Seleccionado */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Ocultar Campos */
+function CamposDetalleVenta(accion) {
+	if(accion == "I"){
+		$("#CamposDetalleVenta").show();
+	} else if (accion == "C") {
+		$("#CamposDetalleVenta").hide();
+	}
+}
+/* FIN - Ocultar Campos */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Calcular Precio Unitario */
+function CalcularPrecioTotal(accion) {
+	var intIdTipoCliente = 1;
+	//var intIdTipoCliente = $("#intIdTipoCliente").val();
+	var intIdProducto = $(accion).attr("idsprt");
+	var dcmDescuentoVenta2 = $("#dcmDescuentoVenta2"+intIdProducto).val();
+	var dcmDescuentoVenta3 = $("#dcmDescuentoVenta3"+intIdProducto).val();
+	var dcmDescuento = $("#dcmDescuento"+intIdProducto).val();
+	var intCantidad = $("#intCantidad"+intIdProducto).val();
+	if((dcmDescuento == "" || dcmDescuento == null) && (intCantidad == "" || intCantidad == null)){
+		return false;
+	} else {
+		if(intIdTipoCliente == 1) {
+			if(Number(dcmDescuento) > dcmDescuentoVenta2) {
+				MensajeNormal("Sobrepasa al descuento 2",2);
+				$("#dcmDescuento"+intIdProducto).val("");
+				return false;
+			}
+		} else if (intIdTipoCliente == 2) {
+			if(Number(dcmDescuento) > dcmDescuentoVenta3) {
+				MensajeNormal("Sobrepasa al descuento 3",2);
+				$("#dcmDescuento"+intIdProducto).val("");
+				return false;
+			}
+		} else {
+			MensajeNormal("Seleccionar un Cliente",2);
+			$("#dcmDescuento"+intIdProducto).val("");
+			return false
+		}
+		var dcmPrecioVenta1 = $("#dcmPrecio"+intIdProducto).val();
+		var dcmPrecioUnitario = (dcmPrecioVenta1 - (dcmPrecioVenta1*(dcmDescuento/100))).toFixed(2);
+		$("#dcmPrecioUnitario"+intIdProducto).val(dcmPrecioUnitario);
+
+		if (intCantidad == "" || intCantidad == null) {
+			$("#dcmTotal"+intIdProducto).val("0.00");
+			CalcularTotal();
+			return false;
+		}
+		else {
+			var dcmTotal = (dcmPrecioUnitario * intCantidad).toFixed(2);
+			$("#dcmTotal"+intIdProducto).val(dcmTotal);
+			CalcularTotal();
+		}
+	}
+}
+/* FIN - Calcular Precio Unitario */
+//////////////////////////////////////////////////////////////
+
+function CalcularTotal(){
+	var VentaTotal = 0.00;
+	var IGVVenta = 0.00;
+	var ValorVenta = 0.00;
+	Number(IGVVenta);
+	Number(VentaTotal);
+	Number(ValorVenta);
+	$('table tbody#ListaDeProductosVender tr').each(function() {
+        $(this).find("td input[name='dcmTotal[]']").each(function() {
+            VentaTotal = VentaTotal + Number(this.value);
+        }); 
+    });
+    ValorVenta = (VentaTotal / 1.18).toFixed(2);
+    IGVVenta = (VentaTotal - ValorVenta).toFixed(2);
+    $("#ValorVenta").val(ValorVenta);
+    $("#IGVVenta").val(IGVVenta);
+	$("#VentaTotal").val(VentaTotal);
+}
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Ver Imagen */
+function VerImagenProducto(seleccion) {
+	var nvchDireccionImg = $(seleccion).attr("imagen");
+	$("#CuadroImagenHeader").css("background-color", "#78909c");
+    $("#CuadroImagenTitulo").css("color", "#FFFFFF");
+    $("#CuadroImagenFooter").css("background-color", "#cfd8dc");
+    $("#CuadroImagenTitulo").html("Imágen del Producto");
+	$("#DireccionImgProducto").html("<img class='img-responsive center-block' src='../../datos/inventario/imgproducto/"+nvchDireccionImg+"' />");
+	$("#CuadroImagen").modal("show");
+}
+/* FIN - Ver Imagen */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Ver Detalle del Ubigeo del Producto Solicitado */
+function VerDetalleUbigeo(seleccion) {
+	var nvchCodigo = $(seleccion).attr("codigo");
+	var intIdProducto = $(seleccion).attr("id");
+	var funcion = "VDU";
+	  $.ajax({
+	   url:"../../datos/inventario/funcion_producto.php",
+	   method:"POST",
+	   data:{intIdProducto:intIdProducto,funcion:funcion},
+	   success:function(datos)
+	   {
+	   	$("#CodigoProducto").html(nvchCodigo);
+	   	$("#DetalleUbigeo").html(datos);
+	   	//goToBox("#TablaDetalleUbigeo");
+	   }
+	  });
+}
+/* FIN - Ver Detalle del Ubigeo del Producto Solicitado */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Ver Detalle del Ubigeo del Producto Solicitado */
+function LimpiarDetalleUbigeo() {
+   	$("#CodigoProducto").html("");
+   	$("#DetalleUbigeo").html("");
+}
+/* FIN - Ver Detalle del Ubigeo del Producto Solicitado */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Listar Domicilios según Ingresa */
+function InsertarCotizacion(seleccion) {
+	var intIdCotizacion = $(seleccion).attr("idct");
+	var funcion = "ICT";
+	var intIdTipoMoneda = $("#tipo-moneda").val();
+	$.ajax({
+	   url:"../../datos/ventas/funcion_venta.php",
+	   method:"POST",
+	   data:{intIdCotizacion:intIdCotizacion,funcion:funcion,intIdTipoMoneda:intIdTipoMoneda},
+	   success:function(datos)
+	   {
+	   	$("#ListaDeProductosComprar").append(datos); 
+	   }
+	});	
+}
+/* FIN - Listar Domicilios según Ingresa */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Funcion Ajax - Listar Producto */
+function PaginacionCotizaciones(seleccion) {
+	var y = 5;
+	var x = $(seleccion).attr("idct") * y;
+	ListarCotizaciones(x,y);
+}
+
+$(document).on('keyup', '#BusquedaCotizacion', function(){
+	var y = 5;
+	var x = 0;
+	ListarCotizaciones(x,y);
+});
+/* FIN - Funciones Ajax - Listar Producto */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Listar Productos para la Selección */
+function ListarCotizaciones(x,y) {
+	var busqueda = document.getElementById("BusquedaCotizacion").value;
+	var funcion = "MCT";
+	var tipolistado = "T";
+	  $.ajax({
+	   url:"../../datos/ventas/funcion_venta.php",
+	   method:"POST",
+	   data:{busqueda:busqueda,funcion:funcion,x:x,y:y,tipolistado:tipolistado},
+	   success:function(datos)
+	   {
+	   	$("#ListaDeCotizaciones").html(datos);
+	   	PaginarCotizaciones((x/y),y);
+	   }
+	});
+}
+/* FIN - Listar Productos para la Selección */
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/* INICIO - Paginar Clientes para la Selección */
+function PaginarCotizaciones(x,y) {
+	var busqueda = document.getElementById("BusquedaProducto").value;
+	var funcion = "PCT";
+	var TipoBusqueda = document.getElementById("tipo-busqueda").value;
+	var tipolistado = "T";
+	  $.ajax({
+	   url:"../../datos/ventas/funcion_venta.php",
+	   method:"POST",
+	   data:{busqueda:busqueda,funcion:funcion,x:x,y:y,tipolistado:tipolistado},
+	   success:function(datos)
+	   {
+	   	$("#PaginacionDeCotizaciones").html(datos);
+	   }
+	  });
+}
+/* FIN - Paginar Clientes para la Selección */
+//////////////////////////////////////////////////////////////
+</script>
--- view/ventas/vcomprobante.php
+++ view/ventas/vcomprobante.php
@@ -7,8 +7,105 @@ require_once '../../datos/conexion/bd_conexion.php';
           $('[data-toggle="tooltip"]').tooltip(); 
       });
     </script>
-    <?php require_once '../../negocio/ventas/nventa.php'; ?>
-    <?php require_once '../../negocio/ventas/ndetalleventa.php'; ?>
+    <script type="text/javascript">
+    var num = 2;
+    var numfila = 0;
+    $(document).on('keyup', '.buscar', function(){
+      $(this).closest('tr').find("input[name='fila[]']").each(function() {
+         numfila = this.value;
+      });
+
+      var search = $(this).val();
+      search = search.replace(/\s/g,'');
+      var funcion = "BP"
+      if(search != '')
+      {
+          $.ajax({
+          type: "POST",
+          url: "../../datos/inventario/funcion_producto.php",
+          data: {search:search,funcion:funcion},
+          cache: false,
+          success: function(html)
+          {
+            $("#result"+numfila).html(html).show();
+          }
+          });
+      }
+      else {
+        $("#result"+numfila).html("").hide();
+      }
+      return false; 
+    });
+
+    $(document).on('click', '.result', function(e){
+      var clicked = $(e.target);
+      var nvchCodigo = clicked.find('.nvchCodigo').html();
+      var intIdProducto = clicked.find('.intIdProducto').val();
+      nvchCodigo = nvchCodigo.replace(/\s/g,'');
+      intIdProducto = intIdProducto.replace(/\s/g,'');
+      $('#nvchCodigo'+numfila).val(nvchCodigo);
+      $(".result").html("").hide();
+      InsertarProductoElegido(intIdProducto,numfila);
+      numfila = 0;
+    });
+
+    $(document).on('click', '.buscar', function(){
+      $(".result").html("").hide();
+    });
+
+    $(document).on('click', function(e){
+      var clicked = $(e.target);
+      if (!clicked.hasClass("buscar")){
+        $(".result").html("").hide();
+      }
+    });
+
+    function InsertarProductoElegido(intIdProducto,id){
+      var funcion = "SP";
+      $.ajax({
+       url:"../../datos/inventario/funcion_producto.php",
+       method:"POST",
+       data:{intIdProducto:intIdProducto,funcion:funcion},
+       dataType:"json",
+       success:function(datos)
+       {
+        $("#dcmPrecio"+id).val(datos.dcmPrecioVenta1);
+        $("#nvchDescripcion"+id).val(datos.nvchDescripcion);
+        $("#dcmDescuentoVenta2"+id).val(datos.dcmDescuentoVenta2);
+        $("#dcmDescuentoVenta3"+id).val(datos.dcmDescuentoVenta3);
+       }
+      });
+    }
+
+    function AgregarFila(){
+      $('#ListaDeProductosVender').append(
+      '<tr>'+
+        '<td><input type="hidden" name="fila[]" value="'+num+'" form="form-venta" />'+
+            '<input type="hidden" id="intIdProducto'+num+'" name="intIdProducto[]" form="form-venta" />'+
+            '<input type="text" class="buscar" id="nvchCodigo'+num+'" name="nvchCodigo[]" form="form-venta" />'+
+            '<div class="result" id="result'+num+'">'+
+        '</td>'+
+        '<td><input type="text" id="nvchDescripcion'+num+'" name="nvchDescripcion[]" form="form-venta" readonly/></td>'+
+        '<td>'+
+          '<input type="text" id="dcmPrecio'+num+'" name="dcmPrecio[]" form="form-venta" readonly />'+
+          '<input type="hidden" id="dcmDescuentoVenta2'+num+'" form="form-venta" readonly />'+
+          '<input type="hidden" id="dcmDescuentoVenta3'+num+'" form="form-venta" readonly />'+
+        '</td>'+
+        '<td><input type="text" id="dcmDescuento'+num+'" name="dcmDescuento[]" form="form-venta" idsprt="'+num+'"'+
+          'onkeyup="CalcularPrecioTotal(this)"/></td>'+
+        '<td><input type="text" id="dcmPrecioUnitario'+num+'" name="dcmPrecioUnitario[]" form="form-venta" readonly/></td>'+
+        '<td><input type="text" id="intCantidad'+num+'" name="intCantidad[]" form="form-venta" idsprt="'+num+'"'+
+          'onkeyup="CalcularPrecioTotal(this)"/></td>'+
+        '<td><input type="text" id="dcmTotal'+num+'" name="dcmTotal[]" form="form-venta" readonly/></td>'+
+        '<td>'+
+          '<button type="button" onclick="EliminarFila(this)" class="btn btn-xs btn-danger"><i class="fa fa-edit"></i> Eliminar</button>'+
+        '</td>'+
+      '</tr>');
+      num++;
+    }
+    </script>
+    <?php require_once '../../negocio/ventas/ncomprobante.php'; ?>
+    <?php require_once '../../negocio/ventas/ndetallecomprobante.php'; ?>
     <?php require_once '../../negocio/operaciones/nvalidaciones.php'; ?>
     <?php require_once '../../negocio/operaciones/nestilos.php'; ?>
     <!--<script type="text/javascript" src="../../negocio/ventas/nventa.js"></script>-->
@@ -16,18 +113,44 @@ require_once '../../datos/conexion/bd_conexion.php';
     <!--<script type="text/javascript" src="../../negocio/operaciones/nvalidaciones.js"></script>-->
     <!--<script type="text/javascript" src="../../negocio/operaciones/nestilos.js"></script>-->
     <style>
-      .pagination a {
-          margin: 0 4px; /* 0 is for top and bottom. Feel free to change it */
-      }
-      hr { 
-          display: block;
-          margin-top: 0.5em;
-          margin-bottom: 0.5em;
-          margin-left: auto;
-          margin-right: auto;
-          border-style: inset;
-          border-width: 1px;
-      }
+    .pagination a {
+        margin: 0 4px; /* 0 is for top and bottom. Feel free to change it */
+    }
+    hr { 
+        display: block;
+        margin-top: 0.5em;
+        margin-bottom: 0.5em;
+        margin-left: auto;
+        margin-right: auto;
+        border-style: inset;
+        border-width: 1px;
+    }
+    .result
+    {
+        position: absolute;
+        z-index: 1;
+        width:500px;
+        padding:10px;
+        display:none;
+        margin-top:-1px;
+        border-top:0px;
+        overflow:hidden;
+        border:1px #CCC solid;
+        background-color: white;
+    }
+    .show
+    {
+        padding:10px;
+        border-bottom:1px solid #000000;
+        font-size:15px; 
+        height:50px;
+    }
+    .show:hover
+    {
+        background:#4c66a4;
+        color:#FFF;
+        cursor:pointer;
+    }
     </style>
   <div class="content-wrapper">
     <section class="content-header">
@@ -133,6 +256,7 @@ require_once '../../datos/conexion/bd_conexion.php';
                   <input type="text" id="nvchNumeracion" name="nvchNumeracion" class="form-control select2" readonly form="form-venta"/>
                 </div>
               </div>
+              <script type="text/javascript">MostrarSeleccionComprobante();</script>
             </div>
             <div class="row">
               <div class="col-md-2">
@@ -154,22 +278,65 @@ require_once '../../datos/conexion/bd_conexion.php';
                     </select>
                 </div>
               </div>
+              <div class="col-md-2">
+                <div class="form-group">
+                  <label>Agregar Fila:</label>
+                  <input type="button" onclick="AgregarFila()" value="Agregar+" class="form-control select2 btn btn-md btn-primary btn-flat" />
+                </div>
+              </div>
             </div>
-            <div class="table-responsive">
-              <table class="table table-hover table-condensed">
-                <thead>
-                <tr>
-                  <th>Código</th>
-                  <th>Descripción</th>
-                  <th>Cantidad</th>
-                  <th>Precio Unit.</th>
-                  <th>Total</th>
-                  <th>Opción</th>
-                </tr>
-                </thead>
-                <tbody id="ListaDeProductosComprar">
-                </tbody>
-              </table>
+            <div class="row">
+              <div class="col-md-10">
+                <div class="table-responsive">
+                  <table class="table table-hover table-condensed">
+                    <thead>
+                    <tr>
+                      <th>Código</th>
+                      <th>Descripción</th>
+                      <th>Precio Lista</th>
+                      <th>Desc. (%)</th>
+                      <th>Precio Unit.</th>
+                      <th>Cantidad</th>
+                      <th>Total</th>
+                      <th>Opciones</th>
+                    </tr>
+                    </thead>
+                    <tbody id="ListaDeProductosVender">
+                      <tr>
+                        <td>
+                          <input type="hidden" name="fila[]" value="1" form="form-venta" />
+                          <input type="hidden" id="intIdProducto1" name="intIdProducto[]" form="form-venta" />
+                          <input type="text" class="buscar" id="nvchCodigo1" name="nvchCodigo[]" form="form-venta" />
+                          <div class="result" id="result1">
+                        </td>
+                        <td><input type="text" id="nvchDescripcion1" name="nvchDescripcion[]" form="form-venta" readonly/></td>
+                        <td>
+                          <input type="text" id="dcmPrecio1" name="dcmPrecio[]" form="form-venta" readonly />
+                          <input type="hidden" id="dcmDescuentoVenta21" form="form-venta" readonly />
+                          <input type="hidden" id="dcmDescuentoVenta31" form="form-venta" readonly />
+                        </td>
+                        <td><input type="text" id="dcmDescuento1" name="dcmDescuento[]" form="form-venta" idsprt="1" 
+                          onkeyup="CalcularPrecioTotal(this)"/></td>
+                        <td><input type="text" id="dcmPrecioUnitario1" name="dcmPrecioUnitario[]" form="form-venta" readonly/></td>
+                        <td><input type="text" id="intCantidad1" name="intCantidad[]" form="form-venta" idsprt="1"
+                          onkeyup="CalcularPrecioTotal(this)"/></td>
+                        <td><input type="text" id="dcmTotal1" name="dcmTotal[]" form="form-venta" readonly/></td>
+                        <td><button type="button" onclick="EliminarFila(this)" class="btn btn-xs btn-danger"><i class="fa fa-edit"></i> Eliminar</button></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div class="form-group">
+                  <label>Valor de Venta:</label>
+                  <input type="text" id="ValorVenta" name="ValorVenta" class="form-control select2" value="S/. 0.00" readonly form="form-venta"/><br>
+                  <label>IGV (18%):</label>
+                  <input type="text" id="IGVVenta" name="IGVVenta" class="form-control select2" value="S/. 0.00" readonly form="form-venta"/><br>
+                  <label>Venta Total:</label>
+                  <input type="text" id="VentaTotal" name="VentaTotal" class="form-control select2" value="S/. 0.00" readonly form="form-venta"/>
+                </div>
+              </div>
             </div>
             <div class="row">
               <div class="col-md-2">
@@ -193,7 +360,7 @@ require_once '../../datos/conexion/bd_conexion.php';
               </div>
             </div>
             <div class="row">
-              <div class="col-md-8">
+              <div class="col-md-7">
                 <div class="form-group">
                   <label>Observación y/o Datos Adicionales (Opcional):</label>
                   <textarea id="nvchObservacion" class="form-control select2" maxlength="800" name="nvchObservacion" form="form-venta" rows="6"></textarea>
