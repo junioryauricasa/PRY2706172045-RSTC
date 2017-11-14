3aa79b4a83063639944d432e2905f2358ee0b718
elShunior <zahoz.moopa@gmail.com>
2017-11-14 02:12:18 -0500

    scrool slim
    - disminuyendo el tamaño del scrooll

--- view/_include/rstheader.php
+++ view/_include/rstheader.php
@@ -59,9 +59,28 @@ if(!isset($_SESSION['intIdUsuarioSesion']))
     background: #EAF1F7;
   }
 }
-  </style>
+  /*
+    *  STYLE for scrool
+  */
+  #scrool-slim::-webkit-scrollbar-track
+  {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    background-color: #F5F5F5;
+  }
+
+  #scrool-slim::-webkit-scrollbar
+  {
+    width: 6px;
+    background-color: #F5F5F5;
+  }
+
+  #scrool-slim::-webkit-scrollbar-thumb
+  {
+    background-color: #000000;
+  }
+</style>
 </head>
-<body class="hold-transition skin-blue fixed sidebar-mini">
+<body class="hold-transition skin-blue fixed sidebar-mini" id="scrool-slim">
 <script type="text/javascript">MostrarUsuarioPerfilNav(<?php echo $_SESSION['intIdUsuarioSesion']; ?>);</script>
 <div class="wrapper">
 
