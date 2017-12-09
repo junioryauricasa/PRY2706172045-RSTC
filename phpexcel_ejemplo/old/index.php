be3dc6b3cd10d4ef9a23afa30bd89c9e6414d26a
hvivanconunez <hvivanconunez@gmail.com>
2017-12-08 19:35:17 -0500

    Excel
    Excel

--- phpexcel_ejemplo/old/index.php
+++ phpexcel_ejemplo/old/index.php
@@ -1,10 +1,17 @@
 <?php
 /** Incluir la libreria PHPExcel */
 require_once 'PHPExcel-1.8/Classes/PHPExcel.php';
-
+require_once '../../datos/conexion/bd_conexion.php';
 // Crea un nuevo objeto PHPExcel
+$sql_conexion = new Conexion_BD();
+$sql_conectar = $sql_conexion->Conectar();
+$sql_comando = $sql_conectar->prepare('CALL buscarproducto_ii(:busqueda,:TipoBusqueda)');
+$sql_comando -> execute(array(':busqueda' => '', ':TipoBusqueda' => 'C'));
+$cantidad = $sql_comando -> rowCount();
+// Establecer propiedades
+$i = 1;
+$fila = $sql_comando -> fetch(PDO::FETCH_ASSOC);
 $objPHPExcel = new PHPExcel();
-
 // Establecer propiedades
 $objPHPExcel->getProperties()
 ->setCreator("Cattivo")
@@ -14,14 +21,32 @@ $objPHPExcel->getProperties()
 ->setDescription("Demostracion sobre como crear archivos de Excel desde PHP.")
 ->setKeywords("Excel Office 2007 openxml php")
 ->setCategory("Pruebas de Excel");
-
+while($fila = $sql_comando -> fetch(PDO::FETCH_ASSOC)){
 // Agregar Informacion
-$objPHPExcel->setActiveSheetIndex(0)
-->setCellValue('A1', 'Valor 1')
-->setCellValue('B1', 'Valor 2')
-->setCellValue('C1', 'Total')
-->setCellValue('A2', '10')
-->setCellValue('C2', '=sum(A2:B2)');
+if($i == 1){
+	$objPHPExcel->setActiveSheetIndex()->getColumnDimension('A')->setWidth(18);
+	$objPHPExcel->setActiveSheetIndex()->getColumnDimension('B')->setWidth(75);
+	$objPHPExcel->setActiveSheetIndex()
+	->setCellValue('A1', 'Código')
+	->setCellValue('B1', 'Descripcion')
+	->setCellValue('C1', 'Precio Venta 1')
+	->setCellValue('D1', 'Precio Venta 2')
+	->setCellValue('E1', 'Precio Venta 3');
+	}
+if($i > 1){
+$objPHPExcel->setActiveSheetIndex()
+->setCellValue('A'.$i, $fila['nvchCodigo'])
+->setCellValue('B'.$i, $fila['nvchDescripcion'])
+->setCellValue('C'.$i, $fila['dcmPrecioVenta1'])
+->setCellValue('D'.$i, $fila['dcmPrecioVenta2'])
+->setCellValue('E'.$i, $fila['dcmPrecioVenta3']);
+}	
+ $i++;
+}
+$objPHPExcel->setActiveSheetIndex()
+->setCellValue('C'.$i, '=SUM(C2:C'.($i-1).')')
+->setCellValue('D'.$i, '=SUM(D2:D'.($i-1).')')
+->setCellValue('E'.$i, '=SUM(E2:E'.($i-1).')');
 
 // Renombrar Hoja
 $objPHPExcel->getActiveSheet()->setTitle('Tecnologia Simple');
